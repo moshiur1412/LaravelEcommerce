e664bc4fa7570cd8248dd3dbb3c3762f87b267ab
vorsurm <moshiur1412@gmail.com>
2020-08-09 11:07:25 +0600

    Testing automatic deployed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel eCommerce 
+                     786 || eCommerce 
                 </div>
 
                <!--  <div class="links">
