384e7922ce865e2da2f541f625300c806550ccd7
vorsurm <moshiur1412@gmail.com>
2020-08-19 12:35:52 +0600

    Implemented UI design for settings

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,7 +50,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path('../public/storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
