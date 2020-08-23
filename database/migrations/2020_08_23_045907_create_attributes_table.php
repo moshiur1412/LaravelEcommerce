fa3363f0f3052ac2bcfec78a2c44daf13d308ed8
vorsurm <moshiur1412@gmail.com>
2020-08-23 11:04:55 +0600

    Create a Attribute Schema

---
+++ database/migrations/2020_08_23_045907_create_attributes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAttributesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attributes', function (Blueprint $table) {
+            $table->id();
+            $table->string('code')->unique();
+            $table->string('name');
+            $table->enum('fronted_type', ['select', 'radio', 'text', 'text area']);
+            $table->boolean('is_filterable')->default(0);
+            $table->boolean('is_required')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attributes');
+    }
+}
