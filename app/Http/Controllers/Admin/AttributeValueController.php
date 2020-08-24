d6968131705719f6b2952730380ba1a8ae18845c
vorsurm <moshiur1412@gmail.com>
2020-08-24 13:10:09 +0600

    Updated AttributeValues vue page

--- app/Http/Controllers/Admin/AttributeValueController.php
+++ app/Http/Controllers/Admin/AttributeValueController.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Contracts\AttributeContract;
 
 class AttributeValueController extends Controller
 {
-    //
+	protected $attributeRepository;
+
+	public function __construct(AttributeContract $attributeRepository){
+		$this->attributeRepository = $attributeRepository;
+	}
+
+	public function getValue(Request $request){
+		$attributeId = $request->input('id');
+		$attribute = $this->attributeRepository->findAttributeById($attributeId);
+		$values = $attribute->values();
+		return response()->json($values);
+	}
+
 }
--- resources/js/components/AttributeValues.vue
+++ resources/js/components/AttributeValues.vue
@@ -1,9 +1,36 @@
 <template>
-<h3>Option Values </h3>
+	<h3>Option Values </h3>
 </template>
 <script>
-export default {
-name: "attribute-values",
-props: ['attributeid'],
+export default{
+	name: "attribute-values",
+	props: ['attributedid'],
+	data(){
+		return {
+			values: [],
+			value: '',
+			price: '',
+			currentId: '',
+			addValue: true,
+			key: 0,
+		}
+	},
+	created: function(){
+		this.loadValues();
+	},
+	methods: {
+		loadValues(){
+			let attributedid = $this.attributedid;
+			let _this = this;
+			axios.post('admin/attributes/get-values', {
+				id: attributesId
+			}).then(function(response)){
+				_this.values = response.data;
+			}.catch(function(error)){
+				console.log(error);
+			}
+
+		}
+	}
 }
 </script>
