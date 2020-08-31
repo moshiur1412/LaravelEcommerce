06960ed372c220d47893db60823904bb60b19039
vorsurm <moshiur1412@gmail.com>
2020-08-31 14:11:55 +0600

    made Brands Model and Migration class

---
+++ app/Models/Brand.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Brand extends Model
+{
+	protected $table = 'brands';
+
+	protected $fillable = ['name', 'slug', 'logo'];
+
+	public function setNameAttribute($value){
+		$this->attributes['name'] = $value;
+		$this->attributes['slug'] = Str::slug($value);
+	}
+}
