90ae8c5695e18d3515f280f0ef95af297a9b5877
vorsurm <moshiur1412@gmail.com>
2020-09-15 13:58:15 +0600

    Fronted Ready after formatting

---
+++ resources/views/site/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>@yield('title') - {{ config('app.name') }} </title>
+    @include('site.partials.styles')
+</head>
+<body>
+    @include('site.partials.header')
+    @yield('content')
+    @include('site.partials.footer')
+    @include('site.partials.scripts')
+    @stack
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'site.pages.homepage');
+Auth::routes();
 
 require 'admin.php';
 
