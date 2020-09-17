a774fc805a3db8730d8f4a0845d85ab46337d269
vorsurm <moshiur1412@gmail.com>
2020-09-17 10:49:11 +0600

    Made Order Migration & Model

---
+++ database/migrations/2020_09_17_042632_create_orders_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('order_number')->unique();
+            $table->unsignedBigInteger('user_id');
+            $table->enum('status', ['pending', 'processing', 'completed', 'decline'])->default('pending');
+            $table->decimal('grand_total', 20, 6);
+            $table->unsignedBigInteger('item_count');
+            $table->boolean('payment_status')->default(1);
+            $table->string('payment_method')->nullable();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('address');
+            $table->string('city');
+            $table->string('country');
+            $table->string('post_code');
+            $table->string('phone_number');
+            $table->text('note');
+
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
