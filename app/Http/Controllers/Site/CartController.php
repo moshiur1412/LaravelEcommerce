5cad924a26a17ef1d297583a4939364f4bb68ea5
vorsurm <moshiur1412@gmail.com>
2020-09-17 10:24:48 +0600

    Remove from item & clear cart method

--- app/Http/Controllers/Site/CartController.php
+++ app/Http/Controllers/Site/CartController.php
@@ -3,11 +3,25 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
+use Cart;
 
 class CartController extends Controller{
 
 	public function getCart(){
 		return view('site.pages.cart');
+	}
+
+	public function removeItem($id){
+		Cart::remove($id);
+		if(Cart::isEmpty()){
+			return redirect('/');
+		}
+		return redirect()->back()->with('message', 'Item removed from cart successfully.');
+	}
+
+	public function clearCart(){
+		Cart::clear();
+		return redirect('/');
 	}
 
 }
