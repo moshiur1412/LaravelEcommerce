7dc3016ce1794b45178ac20a95beb8610761744f
vorsurm <moshiur1412@gmail.com>
2020-09-19 08:31:18 +0600

    Brand Contract Comments added

--- app/Contracts/BrandContract.php
+++ app/Contracts/BrandContract.php
@@ -2,9 +2,33 @@
 namespace App\Contracts;
 
 interface BrandContract{
+	
+	/**
+	* @param string $order
+	* @param string $sort
+	* @param array $columns
+	* @return mixed
+	*/
 	public function listBrands(string $order = 'id', string $sort = 'desc', array $columns = ['*']);
+
+	/**
+	* @param array $params
+	*/
 	public function createBrand(array $params);
+
+	/**
+	* @param int $id
+	* @return mixed
+	*/
 	public function findBrandById(int $id);
+
+	/**
+	* @param array $params
+	*/
 	public function updateBrand(array $params);
+
+	/**
+	* @param int $id
+	*/
 	public function deleteBrand(int $id);
 }
