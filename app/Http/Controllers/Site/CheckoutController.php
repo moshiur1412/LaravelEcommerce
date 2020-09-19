bfa451de30bb23a64861e610479e4f4624ee7acf
vorsurm <moshiur1412@gmail.com>
2020-09-19 08:38:19 +0600

    Checkout Controller Log info added

--- app/Http/Controllers/Site/CheckoutController.php
+++ app/Http/Controllers/Site/CheckoutController.php
@@ -11,18 +11,18 @@ class CheckoutController extends Controller{
 	protected $orderRepository;
 
 	public function __construct(OrderContract $orderRepository){
+		\Log::info("Req=CheckoutController@__construct called");
 		$this->orderRepository = $orderRepository;
 	}
 
 	public function getCheckout(){
+		\Log::info("Req=CheckoutController@getCheckout called");
 		return view('site.pages.checkout');
 	}
 
 	public function placeOrder(Request $request){
-
+		\Log::info("Req=CheckoutController@placeOrder called");
 		$order = $this->orderRepository->storeOrderDetails($request->all());
-
-		dd($request->all());
 		dd($order);
 	}
 }
