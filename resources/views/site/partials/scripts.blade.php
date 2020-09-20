98445686b0ed463dd9fcf1798db133f03a53b0c1
vorsurm <moshiur1412@gmail.com>
2020-09-20 11:53:05 +0600

    Removed scripts space from the code

--- resources/views/site/partials/scripts.blade.php
+++ resources/views/site/partials/scripts.blade.php
@@ -1,9 +1,6 @@
 <script src="{{ asset('frontend/js/jquery-2.0.0.min.js') }}" type="text/javascript"></script>
 <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}" type="text/javascript"></script>
-
 <script src="{{ asset('frontend/plugins/fancybox/fancybox.min.js') }}" type="text/javascript"></script>
-
 <script src="{{ asset('frontend/plugins/owlcarousel/owl.carousel.min.js') }}" type="text/javascript"></script>
-
 <script src="{{ asset('frontend/js/script.js') }}" type="text/javascript"></script>
 
