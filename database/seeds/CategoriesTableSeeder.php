180b6b6fd910ad98960ee6890ed591322fcac6bb
vorsurm <moshiur1412@gmail.com>
2020-09-20 10:20:54 +0600

    Category Table Seeder updated

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -2,9 +2,16 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Category;
+use Faker\Generator as Faker;
 
 class CategoriesTableSeeder extends Seeder
 {
+
+
+    protected $data = [
+        'Colthing'  =>  ["woman Shoes", 'Mens\'s Shirt'],
+        'Handy'     =>  ['Smartphones', 'Smartwatches']
+    ];
     /**
      * Run the database seeds.
      *
@@ -12,13 +19,30 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        Category::create([
-        	'name'			=>	'Root',
-        	'description'	=>	'This is the root category, don\'t delete this one',
-        	'parent_id'		=>	null,
-        	'menu'			=>	0,
-        ]);
 
-        factory('App\Models\Category',10)->create();
+        foreach($this->data as $cat => $subCats){
+            $id = Category::create(['name' => $cat])->id;
+
+            foreach ($subCats as $subCat) {
+
+                Category::create([
+                    'name'          =>  $subCat,
+                    'parent_id'     =>  $id,
+                    'description'   =>  $faker->realText(100),
+                    'menu'          =>  1,
+                ]);
+            }
+
+        }
+        $this->command->info('Inserted '. count($this->data).' records');
+        // Category::create([
+        // 	'name'			=>	'Root',
+        // 	'description'	=>	'This is the root category, don\'t delete this one',
+        // 	'parent_id'		=>	null,
+        // 	'menu'			=>	0,
+        // ]);
+
+        // factory('App\Models\Category')->create();
+
     }
 }
