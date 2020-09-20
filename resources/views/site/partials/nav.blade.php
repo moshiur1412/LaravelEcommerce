9cb01eef249f029ea3a7e79c6c4343fa3aba1319
vorsurm <moshiur1412@gmail.com>
2020-09-20 11:53:20 +0600

    Updated nav for categories

--- resources/views/site/partials/nav.blade.php
+++ resources/views/site/partials/nav.blade.php
@@ -6,26 +6,26 @@
 	</button>
 	<div class="collapse navbar-collapse" id="main_nav">
 		<ul class="navbar-nav">
-			
 			@foreach($categories as $cat)
 			@foreach($cat->items as $category)
-			@if($category->items->count() > 0)
-			<li class="nav-item-dropdown">
-				<a href="{{ route('category.show', $category->slug) }}"  id="{{ $category>slug }}" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" > {{ $category->name }} </a>
-				<div class="dropdown-menu" aria-lableledby="{{ $category->slug }} ">
-					@foreach($category->items as $item )
-					<a href="{{ route('category->show', $item->slug) }} " class="dropdown-item"> {{ $item->name }}</a>
-					@endforeach
-				</div>
-			</li>
-			@else
-			<li class="nav-item">
-				<a class="nav-link" href="{{ route('category.show', $category->slug) }}"> {{ $category->name }} </a>
-			</li>
-			@endif
-			@endforeach
-			@endforeach
-		</ul>
+			@if ($category->items->count() > 0)
+			<li class="nav-item dropdown">
+				<a class="nav-link dropdown-toggle" href="{{ route('category.show', $category->slug) }}" id="{{ $category->slug }}"
+					data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{ $category->name }}</a>
+					<div class="dropdown-menu" aria-labelledby="{{ $category->slug }}">
+						@foreach($category->items as $item)
+						<a class="dropdown-item" href="{{ route('category.show', $item->slug) }}">{{ $item->name }}</a>
+						@endforeach
+					</div>
+				</li>
+				@else
+				<li class="nav-item">
+					<a class="nav-link" href="{{ route('category.show', $category->slug) }}">{{ $category->name }}</a>
+				</li>
+				@endif
+				@endforeach
+				@endforeach
+			</ul>
+		</div>
 	</div>
-</div>
 </nav>
