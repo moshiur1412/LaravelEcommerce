34fecbe6e292d261845c6d8cf5b3f04d787377c0
vorsurm <moshiur1412@gmail.com>
2020-09-21 12:56:39 +0600

    Generated Brand Factory

---
+++ database/factories/BrandFactory.php
@@ -0,0 +1,17 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Brand;
+use Faker\Generator as Faker;
+
+$factory->define(Brand::class, function (Faker $faker) {
+	$brands = ['Nestle', 'PepsiCo, Inc', 'Unilever', 'Pran', 'Halal'];
+
+    	// Brand::create([
+    	// 	'name' 	=>	$faker->randomElement($brands),
+    	// ]);
+	return [
+		'name' => $faker->randomElement($brands)
+	];
+});
