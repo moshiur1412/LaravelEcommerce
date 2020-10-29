784dd5348d1c114be82486581d79704c8fc9ac8d
vorsurm <moshiur1412@gmail.com>
2020-10-29 11:33:48 +0600

    nav menu ui changed

--- resources/views/site/partials/nav.blade.php
+++ resources/views/site/partials/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+<nav class="navbar navbar-main navbar-expand-lg navbar-light border-bottom">
 	<div class="container">
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main_nav"
 		aria-controls="main_nav" aria-expanded="false" aria-label="Toggle navigation">
