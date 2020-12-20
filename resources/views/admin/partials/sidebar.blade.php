b3099d9da8ce3028cbb995c3f90ecb84b9153c18
vorsurm <moshiur1412@gmail.com>
2020-12-20 13:33:56 +0600

    admin sidebar avatar image not found

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,7 +1,7 @@
 <!-- Sidebar menu-->
 <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
 <aside class="app-sidebar">
-	<div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="https://s3.amazonaws.com/uifaces/faces/twitter/jsa/48.jpg" alt="User Image">
+	<div class="app-sidebar__user"> <img class="app-sidebar__user-avatar" src="{{ asset('images/default_avatar.png') }}" alt="User Image">
 		<div>
 			<p class="app-sidebar__user-name">John Doe</p>
 			<p class="app-sidebar__user-designation">Frontend Developer</p>
